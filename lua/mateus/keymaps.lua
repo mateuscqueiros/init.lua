7550e4028553e860396865610cc314743fa23bb0
Mateus Queirós <mateuscqueiros@gmail.com>
2024-01-04 01:51:36 -0300

    Initial - 04/01/24

---
+++ lua/mateus/keymaps.lua
@@ -0,0 +1,19 @@
+local builtin = require('telescope.builtin')
+
+local keymap = vim.keymap
+
+-- ThePrimeagen
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Telescope
+keymap.set('n', '<leader>ff', builtin.find_files, {})
+keymap.set('n', '<leader>fg', builtin.live_grep, {})
+keymap.set('n', '<leader>fb', builtin.buffers, {})
+keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+--Line numbers
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
---
+++ lua/mateus/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return { 
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "tokyonight",
+      },
+    },
+    {
+      "folke/tokyonight.nvim", 
+      opts = {
+        lazy = false,
+        transparent = true,
+        style = "moon",
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        }
+      }
+    }
+}
+    
+

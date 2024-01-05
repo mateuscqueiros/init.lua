2d5259959e574c6a9af1ae8a3883b7ab28edc5da
Mateus Queirós <mateuscqueiros@gmail.com>
2024-01-04 22:08:01 -0300

    Nightly theme

--- lua/mateus/plugins/colorscheme.lua
+++ lua/mateus/plugins/colorscheme.lua
@@ -1,22 +1,29 @@
-return { 
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "tokyonight",
-      },
-    },
-    {
-      "folke/tokyonight.nvim", 
-      opts = {
-        lazy = false,
-        transparent = true,
-        style = "moon",
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        }
-      }
-    }
-}
-    
+tokyonight = { 
+   {
+     "LazyVim/LazyVim",
+     opts = {
+       colorscheme = "tokyonight",
+     },
+   },
+   {
+     "folke/tokyonight.nvim", 
+     opts = {
+       lazy = false,
+       transparent = true,
+       style = "moon",
+       styles = {
+         sidebars = "transparent",
+         floats = "transparent",
+       }
+     }
+   }
+ }
 
+nightly = {
+  "Alexis12119/nightly.nvim",
+  opts = {
+    colorscheme = "nightly",
+  }
+}
+
+return tokyonight

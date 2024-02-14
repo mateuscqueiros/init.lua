48ce098e40800de248c5073a68174eb5eafa1a4e
Mateus Queirós <mateuscqueiros@gmail.com>
2024-02-13 22:56:20 -0300

    More ThePrimeagen configs (error column OH MY GOD)

--- lua/mateus/config.lua
+++ lua/mateus/config.lua
@@ -1,20 +1,36 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.wo.relativenumber = true
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.opt.backspace = "2"
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.autowrite = true
-vim.opt.autoread = true
-vim.opt.autoindent = true
-
--- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.termguicolors = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.terminal_emulator = "tmux"

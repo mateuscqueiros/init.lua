afd3c56a40fe82edfbedc513be9ea9ccfbc94398
Mateus Queirós <mateuscqueiros@gmail.com>
2024-02-15 21:29:33 -0300

    Changed colorscheme structure

--- lua/mateus/plugins/colorscheme.lua
+++ lua/mateus/plugins/colorscheme.lua
@@ -1,92 +1,26 @@
-local tokyonight = {
+return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "solarized-osaka",
     },
   },
   {
     "folke/tokyonight.nvim",
     opts = {
-      lazy = false,
+      style = "night",
     },
-    config = function()
-      require("tokyonight").setup({
-        on_colors = function(colors) colors.border = "#787878" end,
-        transparent = false,
-        style = "night",
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      })
-    end,
   },
-}
-
-local nightly = {
-  "Alexis12119/nightly.nvim",
-  opts = {
-    colorscheme = "nightly",
-  },
-  config = function()
-    require("nightly").setup()
-    vim.cmd.colorscheme("nightly")
-  end,
-}
-
-local solarized = {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    transparent = false,
-  },
-  config = function()
-    require("solarized-osaka").setup({
+  {
+    "craftzdog/solarized-osaka.nvim",
+    opts = {
       transparent = false,
-    })
-    vim.cmd.colorscheme("solarized-osaka")
-  end,
+    },
+  },
+  {
+    "shaunsingh/solarized.nvim",
+    "Alexis12119/nightly.nvim",
+    "rose-pine/neovim",
+    "catppuccin/nvim",
+  },
 }
-
-local onedark = {
-  "navarasu/onedark.nvim",
-  config = function()
-    local onedarktheme = require("onedark")
-
-    onedarktheme.setup({
-      style = "deep",
-    })
-    onedarktheme.load()
-  end,
-}
-
-local rose_pine = {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      disable_background = false,
-    })
-
-    vim.cmd.colorscheme("rose-pine")
-  end,
-}
-
-local catppuccin = {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  config = function() vim.cmd.colorscheme("catppuccin-mocha") end,
-}
-
-local themes = {
-  catppuccin = catppuccin,
-  tokyonight = tokyonight,
-  rose_pine = rose_pine,
-  solarized = solarized,
-  nightly = nightly,
-  onedark = onedark,
-}
-
-return themes.tokyonight
